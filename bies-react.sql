06a293c109979a3872681feb5f08f6eb48ddca74
Juani Greco <juanigreco22@gmail.com>
2022-01-19 18:01:18 -0300

    Arregle el estacionar porq soy buenisimo

--- bies-react.sql
+++ bies-react.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 15-09-2021 a las 23:18:58
+-- Tiempo de generación: 19-01-2022 a las 16:32:55
 -- Versión del servidor: 8.0.21
--- Versión de PHP: 7.3.21
+-- Versión de PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,13 +34,21 @@ CREATE TABLE IF NOT EXISTS `estacionamiento` (
   `idPlayaDeEstacionamiento` int NOT NULL,
   `idPlayaDeEstacionamientoHorario` int NOT NULL,
   `fechaEstacionamiento` date NOT NULL,
-  `horaInicioEstacionamiento` time NOT NULL DEFAULT '00:00:00',
-  `horaFinEstacionamiento` time NOT NULL DEFAULT '00:00:00',
+  `horaInicioEstacionamiento` time NOT NULL,
+  `horaFinEstacionamiento` time DEFAULT NULL,
   PRIMARY KEY (`idEstacionamiento`),
   KEY `fk_idPlayaDeEstacionamiento` (`idPlayaDeEstacionamiento`),
   KEY `fk_idPlayaDeEstacionamientoHorario` (`idPlayaDeEstacionamientoHorario`),
   KEY `fk_idUsuario` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `estacionamiento`
+--
+
+INSERT INTO `estacionamiento` (`idEstacionamiento`, `idUsuario`, `idPlayaDeEstacionamiento`, `idPlayaDeEstacionamientoHorario`, `fechaEstacionamiento`, `horaInicioEstacionamiento`, `horaFinEstacionamiento`) VALUES
+(8, 1, 1, 16, '2021-09-21', '11:30:00', NULL),
+(10, 2, 2, 1, '2021-09-20', '11:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -57,7 +65,15 @@ CREATE TABLE IF NOT EXISTS `playadeestacionamiento` (
   `observaciones` varchar(2000) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `mapa` varchar(10000) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`idPlayaDeEstacionamiento`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `playadeestacionamiento`
+--
+
+INSERT INTO `playadeestacionamiento` (`idPlayaDeEstacionamiento`, `nombrePlayaDeEstacionamiento`, `ubicacion`, `capacidad`, `observaciones`, `mapa`) VALUES
+(1, 'Supermercado Dar', 'San Jeronimo y Tucuman', 10, 'holis', NULL),
+(2, 'Estacionamiento Tucuman', 'Estacionamiento Tucuman y San Martin', 100, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -68,11 +84,47 @@ CREATE TABLE IF NOT EXISTS `playadeestacionamiento` (
 DROP TABLE IF EXISTS `playadeestacionamientohorario`;
 CREATE TABLE IF NOT EXISTS `playadeestacionamientohorario` (
   `idHorario` int NOT NULL AUTO_INCREMENT,
+  `idPlayaDeEstacionamiento` int NOT NULL,
   `diaSemana` int NOT NULL,
   `horaInicio` time NOT NULL,
   `horaFin` time NOT NULL,
-  PRIMARY KEY (`idHorario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  PRIMARY KEY (`idHorario`),
+  KEY `fk_idPlayaDeEstacionamiento2` (`idPlayaDeEstacionamiento`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `playadeestacionamientohorario`
+--
+
+INSERT INTO `playadeestacionamientohorario` (`idHorario`, `idPlayaDeEstacionamiento`, `diaSemana`, `horaInicio`, `horaFin`) VALUES
+(1, 2, 0, '08:00:00', '13:00:00'),
+(2, 2, 1, '08:00:00', '13:00:00'),
+(3, 2, 2, '08:00:00', '13:00:00'),
+(4, 2, 3, '08:00:00', '13:00:00'),
+(5, 2, 4, '08:00:00', '13:00:00'),
+(6, 2, 5, '08:00:00', '13:00:00'),
+(7, 2, 6, '08:00:00', '13:00:00'),
+(8, 2, 0, '16:00:00', '20:00:00'),
+(9, 2, 1, '16:00:00', '20:00:00'),
+(10, 2, 2, '16:00:00', '20:00:00'),
+(11, 2, 3, '16:00:00', '20:00:00'),
+(12, 2, 4, '16:00:00', '20:00:00'),
+(13, 2, 5, '16:00:00', '20:00:00'),
+(14, 2, 6, '16:00:00', '20:00:00'),
+(15, 1, 0, '08:00:00', '12:00:00'),
+(16, 1, 1, '08:00:00', '12:00:00'),
+(17, 1, 2, '08:00:00', '12:00:00'),
+(18, 1, 3, '08:00:00', '12:00:00'),
+(19, 1, 4, '08:00:00', '12:00:00'),
+(20, 1, 5, '08:00:00', '12:00:00'),
+(21, 1, 6, '08:00:00', '12:00:00'),
+(22, 1, 0, '16:00:00', '21:00:00'),
+(23, 1, 1, '16:00:00', '21:00:00'),
+(24, 1, 2, '16:00:00', '21:00:00'),
+(25, 1, 3, '16:00:00', '21:00:00'),
+(26, 1, 4, '16:00:00', '21:00:00'),
+(27, 1, 5, '16:00:00', '21:00:00'),
+(28, 1, 6, '16:00:00', '21:00:00');
 
 -- --------------------------------------------------------
 
@@ -106,12 +158,12 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `idUsuario` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `dni` int NOT NULL,
-  `clave` varchar(300) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `clave` varchar(1000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `idRol` int NOT NULL DEFAULT '2',
   PRIMARY KEY (`idUsuario`),
   KEY `fk_idRol` (`idRol`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -120,7 +172,9 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 INSERT INTO `usuarios` (`idUsuario`, `nombre`, `dni`, `clave`, `email`, `idRol`) VALUES
 (1, 'Guido ', 37774564, 'guido', 'guido_magi@hotmail.com', 1),
 (2, 'juani', 1, '2', 'email@email.com', 2),
-(21, 'Prueba 2', 10, '$2y$10$nWfFc/qpnmaPuv0xqN8m9.GEsGGPCIMdrlrmXWdC/pUd1VaHI3R2C', 'probando', 2);
+(3, 'Prueba 2', 10, '$2y$10$nWfFc/qpnmaPuv0xqN8m9.GEsGGPCIMdrlrmXWdC/pUd1VaHI3R2C', 'probando', 2),
+(4, 'Juani Prueba', 22, 'prueba', 'prueba@probando.com', 2),
+(27, 'Guido', 123, '$2y$10$NEFK1QpZcR4GYDbu5TWz5ebdGn9tpfreqtfywe6AIck7Refphzmi6', 'probando', 2);
 
 --
 -- Restricciones para tablas volcadas
@@ -133,6 +187,12 @@ ALTER TABLE `estacionamiento`
   ADD CONSTRAINT `fk_idPlayaDeEstacionamiento` FOREIGN KEY (`idPlayaDeEstacionamiento`) REFERENCES `playadeestacionamiento` (`idPlayaDeEstacionamiento`),
   ADD CONSTRAINT `fk_idPlayaDeEstacionamientoHorario` FOREIGN KEY (`idPlayaDeEstacionamientoHorario`) REFERENCES `playadeestacionamientohorario` (`idHorario`),
   ADD CONSTRAINT `fk_idUsuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`);
+
+--
+-- Filtros para la tabla `playadeestacionamientohorario`
+--
+ALTER TABLE `playadeestacionamientohorario`
+  ADD CONSTRAINT `fk_idPlayaDeEstacionamiento2` FOREIGN KEY (`idPlayaDeEstacionamiento`) REFERENCES `playadeestacionamiento` (`idPlayaDeEstacionamiento`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Filtros para la tabla `usuarios`
--- estacionar.php
+++ estacionar.php
@@ -1,52 +1,94 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET,POST");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+//Permite el acceso desde cualquier origen
+header('Access-Control-Allow-Origin: *');
+
+//Conexion a la base de datos MySql
+include "conectar.php";
+$conn = conectarDB();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "pase la conexion::::  ";
 
 
-// Conecta a la base de datos  con usuario, contraseña y nombre de la BD
-$servidor = "localhost"; $usuario = "root"; $contrasenia = ""; $nombreBaseDatos = "bies-react";
-$conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
+//Definicion de variables recibidas del post
 
+//--------En produccion-------
+
+/* $idUsuario= $_POST['Id_Usuario']; 
+date_default_timezone_set("America/Argentina/Buenos_Aires");
+$horaActual = date_create();
+$horaActual = date_format($horaActual, 'H:i:s');
+$fechaActual = date("Y-m-d");
+$idPlayaDeEstacionamiento = $_POST['idPlayaEstacionamiento'];
+$idEstacionamientoHorario = $_POST('idHorario');
+$idPlayaDeEstacionamientoHorario = $_POST('idPlayaEstacionamientoHorario');*/
 
 
 //---------En prueba----------
+
 $idUsuario = 3;
-$HoraActual = date_create();
-$HoraActual = '11:30:20';
-$FechaActual = date("Y-m-d");
-$idPlayaDeEstacionamiento = '12';
+date_default_timezone_set("America/Argentina/Buenos_Aires");
+$horaActual = date_create();
+$horaActual = date_format($horaActual, 'H:i:s');
+$fechaActual = date("Y-m-d");
+$idPlayaDeEstacionamiento = '1';
 $diaSemana = date('w');
 
-//if(isset($_GET["insertar"])){
-    $data = json_decode(file_get_contents("php://input"));
-    date_default_timezone_set("America/Argentina/Buenos_Aires");
-    
-    /*$HoraActual=$data-> date_create();
-    $HoraActual=$data-> date_format($HoraActual, 'H:i:s');
-    $FechaActual=$data-> date("Y-m-d");
-    $idPlayaDeEstacionamiento=$data-> idPlayaDeEstacionamiento;
-    $idEstacionamientoHorario=$data-> idEstacionamientoHorario;
-    $idPlayaDeEstacionamientoHorario=$data-> idPlayaDeEstacionamientoHorario;*/
 
-   
 
-    if (($HoraActual > "00:00:00") and ($HoraActual < "23:59:59")) {
-        $sql = mysqli_query($conexionBD,"INSERT INTO `estacionamiento`(`idUsuario`, `idPlayaDeEstacionamiento`, `idPlayaDeEstacionamientoHorario`, `fechaEstacionamiento`, 
+
+//error_log ($idPlayaDeEstacionamientoHorario, 3, 'D:\ID.txt');
+//Consulta sql para estacionar un vehiculo EN EL ESTACIONAMIENTO CON ID = 1;
+
+
+if (($horaActual > "00:00:00") and ($horaActual < "23:59:59")) {
+        $sql = "INSERT INTO `estacionamiento`(`idUsuario`, `idPlayaDeEstacionamiento`, `idPlayaDeEstacionamientoHorario`, `fechaEstacionamiento`, 
         `horaInicioEstacionamiento`) 
-        VALUES ($idUsuario, $idPlayaDeEstacionamiento, '(SELECT playadeestacionamientohorario.idHorario
+        VALUES ($idUsuario, $idPlayaDeEstacionamiento, (SELECT playadeestacionamientohorario.idHorario
         FROM `playadeestacionamientohorario` 
-        WHERE '$HoraActual' BETWEEN playadeestacionamientohorario.horaInicio and playadeestacionamientohorario.horaFin 
-        and playadeestacionamientohorario.diaSemana = $diaSemana and playadeestacionamientohorario.idPlayaDeEstacionamiento = $idPlayaDeEstacionamiento)',
-        '$FechaActual', '$HoraActual'"); 
-
-        echo json_encode(["success"=>1]);
-    }
-   
-    exit();
-//}
+        WHERE '$horaActual' BETWEEN playadeestacionamientohorario.horaInicio and playadeestacionamientohorario.horaFin 
+        and playadeestacionamientohorario.diaSemana = $diaSemana and playadeestacionamientohorario.idPlayaDeEstacionamiento = $idPlayaDeEstacionamiento),
+        '$fechaActual', '$horaActual')";
+} else {
+};
 
 
+/*
+error_log ($idUsuario, 3, 'D:\ID.txt');
+error_log ($horaActual, 3, 'D:\hora.txt');
+error_log ($fechaActual, 3, 'D:\fecha.txt');
+error_log ($diaSemana, 3, 'D:\dia.txt');
+error_log($idPlayaDeEstacionamiento, 3, 'D:\idPlaya.txt');
+*/
 
+
+header('Content-Type: application/json');
+//Ejecuta la consulta
+if ($resultado = $conn->query($sql)) {
+    echo json_encode('Ok', JSON_FORCE_OBJECT);
+} else {
+    echo json_encode('ERROR', JSON_FORCE_OBJECT);
+}
+//echo ("pase la ejecucion");
+
+//Arma la cabecera "json"
+header('Content-Type: application/json');
+
+//Verifica si encontro filas en la tabla
+//if ($resultado->num_rows > 0) {
+//El resultado (resulset) se recorre con el while	
+//A cada fila (en este caso es una sola) la carga en un array 
+//    while ($fila=mysqli_fetch_array($resultado)){
+//    $usuario = array ('Id_usuario' => $fila['ID_USUARIO']);
+//    }
+//} else {
+//Si no existe crea el array con todos valores vacios	
+//    $usuario = array ('Id_usuario'=>'');
+//}	
+
+//Codifica y retorna en formato json el array
+
+//cierra la conexion
+$conn->close();
